6e272c01fdc77cf5f2bd42ef3bcdf41cde6a4687
AndrewMMickel <andrew.m.mickel@gmail.com>
2021-04-20 15:54:11 -0700

    added edit and delete functions for views

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -40,5 +40,35 @@ namespace HairSalon.Controllers
             Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
             return View(thisClient);
         }
+
+        public ActionResult Edit(int id)
+        {
+            Client thisClient = _db.Clients.Include(client => client.Stylist).FirstOrDefault(Client => Client.ClientId == id);
+            ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+            return View(thisClient);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Client client)
+        {
+            _db.Entry(client).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Client thisClient = _db.Clients.FirstOrDefault(Client => Client.ClientId == id);
+            return View(thisClient);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Client thisClient = _db.Clients.FirstOrDefault(Client => Client.ClientId == id);
+            _db.Clients.Remove(thisClient);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
--- Controllers/StylistsController.cs
+++ Controllers/StylistsController.cs
@@ -2,6 +2,8 @@ using HairSalon.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HairSalon.Controllers
 {
@@ -34,8 +36,38 @@ namespace HairSalon.Controllers
 
         public ActionResult Details(int id)
         {
-            Stylist thisStylist = _db.Stylists.FirstOrDefault(stylists => stylists.StylistId == id);
+            Stylist thisStylist = _db.Stylists.Include(stylist => stylist.Clients).FirstOrDefault(stylists => stylists.StylistId == id);
             return View(thisStylist);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Stylist thisStylist = _db.Stylists.Include(stylist => stylist.Clients).FirstOrDefault(stylists => stylists.StylistId == id);
+            ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name", "Specialty");
+            return View(thisStylist);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Stylist stylist)
+        {
+            _db.Entry(stylist).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Stylist thisStylist = _db.Stylists.FirstOrDefault(Stylist => Stylist.StylistId == id);
+            return View(thisStylist);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Stylist thisStylist = _db.Stylists.FirstOrDefault(Stylist => Stylist.StylistId == id);
+            _db.Stylists.Remove(thisStylist);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
